b4cf23de4e8283d389d588de3df6dc5763aa2ceb
tanergokcek <tanergokcek1@outlook.com>
2023-10-23 23:56:47 +0300

    java-A3Degiskenler

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int birinciSayi = 15 ;                              //Degisken yazilirken ilk harf kücük ikinci büyük.
+        int ikinciSayi ;                                    //Degerini sonradan tanimlayabiliriz.
+
+        ikinciSayi = 30 ;
+
+        birinciSayi= 50;                                    //Tanimladigimiz degeri degistirebiliriz.Ve bunu algılar.
+
+        int sayil =10, sayi2=20 ;                          //Tek satirda tanimlayabiliriz.
+
+        System.out.println("Birinci sayinin degeri : " + birinciSayi + " ikinci sayi :" + ikinciSayi ); //ortadaki +
+
+
+    }
+}
